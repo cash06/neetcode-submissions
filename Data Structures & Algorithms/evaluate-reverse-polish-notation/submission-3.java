de018a29221bfeb782d5d420f1a2cbae22e98e0a
Cash Pergande <cash.pergande@gmail.com>
2026-09-22 13:31:59 -0500

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        for (String t : tokens) {
+            if (!"*/+-".contains(t)) {
+                stack.push(Integer.valueOf(t));
+                continue;
+            }
+            int num2 = stack.pop();
+            int num1 = stack.pop();
+            stack.push(helper(t, num1, num2));
+        }
+        return stack.pop();
+    }
+    private int helper(String t, int num1, int num2) {
+        if (t.equals("+")) {
+            return num1 + num2;
+        } else if (t.equals("-")) {
+            return num1 - num2;
+        } else if (t.equals("*")) {
+            return num1 * num2;
+        } else {
+            return num1 / num2;
+        }
+    }
+}
